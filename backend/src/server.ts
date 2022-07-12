27a37b2e4c25e29579094b969e24370e6933c0d1
LuizGambalonga <gambalongaluizhenrique@gmail.com>
2022-07-12 20:38:04 -0300

    First Commit

---
+++ backend/src/server.ts
@@ -0,0 +1,19 @@
+import express, {Request,Response,NextFunction, json } from "express";
+import 'express-async-errors';
+
+import { router } from "./routes";
+
+const app = express();
+app.use(router);
+app.use((err: Error, req: Request, res: Response, next : NextFunction)=>{
+    if(err instanceof Error){
+        return res.status(400).json({error: err.message})
+    }
+
+return res.status(500).json({status: 'Error'})
+})
+app.use(express.json());
+
+app.listen(3333, ()=>{
+    console.log("Server On!")
+})
